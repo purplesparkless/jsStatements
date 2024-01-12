917167dec42902f60c74a9cfc63fae0e27b6ab9d
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-12 23:29:49 +0100

    statement commit

---
+++ main.js
@@ -0,0 +1,46 @@
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+
+// JavaScript statements are composed of:
+// Values, Operators, Expressions, Keywords, and Comments.
+// This statement tells the browser to write "Hello Dolly." inside an HTML element with id="demo":
+
+document.getElementById("demo").innerHTML = "Hello Dolly."; 
+
+
+// Semicolons ;
+// Semicolons separate JavaScript statements.
+// Add a semicolon at the end of each executable statement:
+Examples
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+// When separated by semicolons, multiple statements on one line are allowed:
+a = 5; b = 6; c = a + b;
+
+
+// JavaScript White Space
+// JavaScript ignores multiple spaces. You can add white space to your script to make it more readable.
+// The following lines are equivalent:
+let person = "Hege";
+// let person="Hege";
+
+// A good practice is to put spaces around operators ( = + - * / ):
+let x = y + z;
+
+
+// JavaScript Keywords
+// Keyword 	Description
+// var 	Declares a variable
+// let 	Declares a block variable
+// const 	Declares a block constant
+// if 	Marks a block of statements to be executed on a condition
+// switch 	Marks a block of statements to be executed in different cases
+// for 	Marks a block of statements to be executed in a loop
+// function 	Declares a function
+// return 	Exits a function
+// try 	Implements error handling to a block of statements
