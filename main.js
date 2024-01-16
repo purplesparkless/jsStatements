1125b7a06e8520287dccce7f88306a40ddcfd7e8
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-16 23:00:05 +0100

    new commit

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ a = 5; b = 6; c = a + b;
 // JavaScript White Space
 // JavaScript ignores multiple spaces. You can add white space to your script to make it more readable.
 // The following lines are equivalent:
-let person = "Hege";
-// let person="Hege";
+let person = "Ade";
+// let person="Ade";
 
 // A good practice is to put spaces around operators ( = + - * / ):
 let x = y + z;
